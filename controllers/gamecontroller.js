bb86a3abf3117f87122db336a872b40c137eb241
Vladimir Kuleshov <kyleshovbb@gmail.com>
2021-05-21 01:51:07 +0300

    fix: update get all games route

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -3,34 +3,26 @@ const Game = require('../db').import('../models/game');
 
 router.get('/all', (req, res) => {
     Game.findAll({ where: { owner_id: req.user.id } })
-        .then(
-            function findSuccess(data) {
-                res.status(200).json({ games })
-            },
-
-            function findFail() {
-                res.status(500).json({
-                    message: "Data not found"
-                })
-            }
-        )
+        .then((games) => {
+            res.status(200).json({ games })
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "Games is not defined"
+            })
+        })
 })
 
 router.get('/:id', (req, res) => {
     Game.findOne({ where: { id: req.params.id, owner_id: req.user.id } })
-        .then(
-            function findSuccess(game) {
-                res.status(200).json({
-                    game: game
-                })
-            },
-
-            function findFail(err) {
-                res.status(500).json({
-                    message: "Data not found."
-                })
-            }
-        )
+        .then((game) => {
+            res.status(200).json({ game })
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: "Game not found"
+            })
+        })
 })
 
 router.post('/create', (req, res) => {
@@ -63,13 +55,13 @@ router.put('/update/:id', (req, res) => {
         {
             where: {
                 id: req.params.id,
-                owner_id: req.user
+                owner_id: req.user.id
             }
         })
         .then(
             function updateSuccess(game) {
                 res.status(200).json({
-                    game: game,
+                    game,
                     message: "Successfully updated."
                 })
             },
@@ -93,7 +85,7 @@ router.delete('/remove/:id', (req, res) => {
     .then(
         function deleteSuccess(game) {
             res.status(200).json({
-                game: game,
+                game,
                 message: "Successfully deleted"
             })
         },
